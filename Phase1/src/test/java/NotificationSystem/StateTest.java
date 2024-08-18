e3b41d9f19f186575425afc1a0dbdb0a19c5d8ca
mahdi <mahdi13812002@gmail.com>
2024-08-18 17:47:13 +0330

    add tests to pass for handling different state

---
+++ Phase1/src/test/java/NotificationSystem/StateTest.java
@@ -0,0 +1,42 @@
+package NotificationSystem;
+import graph.Graph;
+import graph.Node;
+import graph.Edge;
+import org.junit.jupiter.api.Test;
+import state.BidirectionalState;
+import state.State;
+import state.UnidirectionalState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StateTest {
+
+    @Test
+    public void testToggleToUnidirectional() {
+        Node cityA = new Node();
+        Node cityB = new Node();
+
+        Edge.createEdge(cityA, cityB, false, 1);
+
+        Graph graph = new Graph(new ArrayList<>(List.of(cityA, cityB)));
+        NotificationSystem context = new NotificationSystem();
+        context.setState(new UnidirectionalState());
+        context.toggleDirection(graph);
+        assertTrue(cityA.getEdges().get(0).isDirected());
+    }
+
+    @Test
+    public void testToggleToBidirectional() {
+        Node cityA = new Node();
+        Node cityB = new Node();
+        Edge.createEdge(cityA, cityB, true, 1);  // Initially unidirectional
+        Graph graph = new Graph(new ArrayList<>(List.of(cityA, cityB)));
+        NotificationSystem context = new NotificationSystem();
+        context.setState(new BidirectionalState());
+        context.toggleDirection(graph);
+        assertFalse(cityA.getEdges().get(0).isDirected());
+    }
+}

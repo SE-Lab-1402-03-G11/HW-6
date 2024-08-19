4d7089e67ad6e3cc7c08c52281eb2a35bf7c695d
behzadnabawi <behzadnabawi@gmail.com>
2024-08-19 17:02:36 +0330

    Implement facade pattern CodeGeneratorFacade

---
+++ Phase2/src/main/java/MiniJava/codeGenerator/CodeGeneratorFacade.java
@@ -0,0 +1,19 @@
+package MiniJava.codeGenerator;
+
+import MiniJava.scanner.token.Token;
+
+public class CodeGeneratorFacade {
+    private CodeGenerator codeGenerator;
+
+    public CodeGeneratorFacade(){
+        codeGenerator = new CodeGenerator();
+    }
+
+    public void semanticFunction(int func, Token next){
+        codeGenerator.semanticFunction(func, next);
+    }
+
+    public void printMemory(){
+        codeGenerator.printMemory();
+    }
+}
